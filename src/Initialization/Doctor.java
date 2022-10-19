28cde2ebdcd02f6b3bf5adc047868cf71e7217e1
jeeeeeeremy <wang.yibin@northeastern.edu>
2022-10-19 10:22:39 -0400

    models

---
+++ src/Initialization/Doctor.java
@@ -0,0 +1,41 @@
+package Initialization;
+
+import java.util.ArrayList;
+
+public class Doctor {
+    String Name;
+    String Hospital;
+    ArrayList<String> Diagnose;
+
+    public Doctor() {
+    }
+
+    public Doctor(String name, String hospital) {
+        Name = name;
+        Hospital = hospital;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getHospital() {
+        return Hospital;
+    }
+
+    public ArrayList<String> getDiagnose() {
+        return Diagnose;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setHospital(String hospital) {
+        Hospital = hospital;
+    }
+
+    public void addDiagnose(String diagnose) {
+        Diagnose.add(diagnose);
+    }
+}
---
+++ src/Initialization/Main.java
@@ -0,0 +1,19 @@
+package Initialization;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main {
+    //doctor set
+    HashSet<Doctor> doctors;
+    //<hospitalName, DoctorSet>
+    HashMap<String,HashSet> hospital;
+    //<communityName, hospitalName>
+    HashMap<String,String> communityDirectory;
+    //<cityName,communityName>
+    HashMap<String,String> cityDirectory;
+
+    public static void main(String[] args) {
+        //initialize all the three login UIs
+    }
+}
