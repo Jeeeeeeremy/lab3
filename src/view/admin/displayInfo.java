72a932b3219993a623aec1e4fd614e36cceb6c00
jeeeeeeremy <wang.yibin@northeastern.edu>
2022-10-31 11:28:40 -0400

    delete button

--- src/view/admin/displayInfo.java
+++ src/view/admin/displayInfo.java
@@ -4,6 +4,8 @@
 
 package view.admin;
 
+import java.awt.event.*;
+import java.util.*;
 import model.Patient;
 
 import javax.swing.*;
@@ -44,10 +46,23 @@ public class displayInfo extends JFrame {
         };
         scrollPane1.setViewportView(table1);
     }
+
+    private void delete(ActionEvent e) {
+        // TODO add your code here
+        int selected_row = table1.getSelectedRow();
+        if (selected_row < 0) {
+            JOptionPane.showMessageDialog(new JDialog(), ":please select one row to delete");
+            return;
+        }
+        patients.remove(selected_row);
+        preparetable();
+    }
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        ResourceBundle bundle = ResourceBundle.getBundle("form");
         scrollPane1 = new JScrollPane();
         table1 = new JTable();
+        delete = new JButton();
 
         //======== this ========
         var contentPane = getContentPane();
@@ -57,20 +72,31 @@ public class displayInfo extends JFrame {
             scrollPane1.setViewportView(table1);
         }
 
+        //---- delete ----
+        delete.setText(bundle.getString("displayInfo.delete.text"));
+        delete.addActionListener(e -> delete(e));
+
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
             contentPaneLayout.createParallelGroup()
                 .addGroup(contentPaneLayout.createSequentialGroup()
-                    .addContainerGap()
-                    .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 1013, GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(4, Short.MAX_VALUE))
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 787, GroupLayout.PREFERRED_SIZE))
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(68, 68, 68)
+                            .addComponent(delete)))
+                    .addContainerGap(60, Short.MAX_VALUE))
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
                 .addGroup(contentPaneLayout.createSequentialGroup()
-                    .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 701, GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 3, Short.MAX_VALUE))
+                    .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 448, GroupLayout.PREFERRED_SIZE)
+                    .addGap(18, 18, 18)
+                    .addComponent(delete)
+                    .addGap(0, 8, Short.MAX_VALUE))
         );
         pack();
         setLocationRelativeTo(getOwner());
@@ -80,5 +106,6 @@ public class displayInfo extends JFrame {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     private JScrollPane scrollPane1;
     private JTable table1;
+    private JButton delete;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
